a7875fe70bbb61e4fd06288259d29347f20ac9f0
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2020-01-20 22:12:32 +0200

    174 : Views created as empty for support tables

---
+++ MovieStore/MovieStore.Desktop/Views/PublisherList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MovieStore.Desktop.Views
+{
+    /// <summary>
+    /// Interaction logic for PublisherList.xaml
+    /// </summary>
+    public partial class PublisherList : UserControl
+    {
+        public PublisherList()
+        {
+            InitializeComponent();
+        }
+    }
+}
